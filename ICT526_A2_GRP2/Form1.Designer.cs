dae8725dd995bfd7e623cef4dcd6ded3d5a23f9f
Rheginald GREGORIO <rheginald.gregorio@gmail.com>
2020-10-07 12:13:17 +1300

    made naming conventions

--- ICT526_A2_GRP2/Form1.Designer.cs
+++ ICT526_A2_GRP2/Form1.Designer.cs
@@ -28,208 +28,208 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnUpdate = new System.Windows.Forms.Button();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.dataInv = new System.Windows.Forms.DataGridView();
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.txtdisc = new System.Windows.Forms.TextBox();
-            this.txtprc = new System.Windows.Forms.TextBox();
-            this.txtCol = new System.Windows.Forms.TextBox();
-            this.txtQuan = new System.Windows.Forms.TextBox();
-            this.txtCode = new System.Windows.Forms.TextBox();
-            this.txtName = new System.Windows.Forms.TextBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataInv)).BeginInit();
-            this.groupBox1.SuspendLayout();
+            this.btnIMUpdate1 = new System.Windows.Forms.Button();
+            this.lblIMdisc1 = new System.Windows.Forms.Label();
+            this.lblIMprice1 = new System.Windows.Forms.Label();
+            this.lblIMCol1 = new System.Windows.Forms.Label();
+            this.lblIMQuan1 = new System.Windows.Forms.Label();
+            this.lblIMItemCode1 = new System.Windows.Forms.Label();
+            this.lblIMName1 = new System.Windows.Forms.Label();
+            this.dataIMInv1 = new System.Windows.Forms.DataGridView();
+            this.btnIMAdd1 = new System.Windows.Forms.Button();
+            this.txtIMdisc1 = new System.Windows.Forms.TextBox();
+            this.txtIMprc1 = new System.Windows.Forms.TextBox();
+            this.txtIMcol1 = new System.Windows.Forms.TextBox();
+            this.txtIMQuan1 = new System.Windows.Forms.TextBox();
+            this.txtIMCode1 = new System.Windows.Forms.TextBox();
+            this.txtIMName1 = new System.Windows.Forms.TextBox();
+            this.grpIMadd = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataIMInv1)).BeginInit();
+            this.grpIMadd.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btnUpdate
+            // btnIMUpdate1
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(713, 469);
-            this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
-            this.btnUpdate.TabIndex = 32;
-            this.btnUpdate.Text = "Update Item";
-            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnIMUpdate1.Location = new System.Drawing.Point(713, 469);
+            this.btnIMUpdate1.Name = "btnIMUpdate1";
+            this.btnIMUpdate1.Size = new System.Drawing.Size(75, 23);
+            this.btnIMUpdate1.TabIndex = 32;
+            this.btnIMUpdate1.Text = "Update Item";
+            this.btnIMUpdate1.UseVisualStyleBackColor = true;
             // 
-            // label6
+            // lblIMdisc1
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(356, 58);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(47, 13);
-            this.label6.TabIndex = 28;
-            this.label6.Text = "discount";
+            this.lblIMdisc1.AutoSize = true;
+            this.lblIMdisc1.Location = new System.Drawing.Point(356, 58);
+            this.lblIMdisc1.Name = "lblIMdisc1";
+            this.lblIMdisc1.Size = new System.Drawing.Size(47, 13);
+            this.lblIMdisc1.TabIndex = 28;
+            this.lblIMdisc1.Text = "discount";
             // 
-            // label5
+            // lblIMprice1
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(356, 17);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(33, 13);
-            this.label5.TabIndex = 27;
-            this.label5.Text = "price ";
+            this.lblIMprice1.AutoSize = true;
+            this.lblIMprice1.Location = new System.Drawing.Point(356, 17);
+            this.lblIMprice1.Name = "lblIMprice1";
+            this.lblIMprice1.Size = new System.Drawing.Size(33, 13);
+            this.lblIMprice1.TabIndex = 27;
+            this.lblIMprice1.Text = "price ";
             // 
-            // label4
+            // lblIMCol1
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(197, 58);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(31, 13);
-            this.label4.TabIndex = 26;
-            this.label4.Text = "Color";
+            this.lblIMCol1.AutoSize = true;
+            this.lblIMCol1.Location = new System.Drawing.Point(197, 58);
+            this.lblIMCol1.Name = "lblIMCol1";
+            this.lblIMCol1.Size = new System.Drawing.Size(31, 13);
+            this.lblIMCol1.TabIndex = 26;
+            this.lblIMCol1.Text = "Color";
             // 
-            // label3
+            // lblIMQuan1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(197, 16);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 13);
-            this.label3.TabIndex = 25;
-            this.label3.Text = "Quantity";
+            this.lblIMQuan1.AutoSize = true;
+            this.lblIMQuan1.Location = new System.Drawing.Point(197, 16);
+            this.lblIMQuan1.Name = "lblIMQuan1";
+            this.lblIMQuan1.Size = new System.Drawing.Size(46, 13);
+            this.lblIMQuan1.TabIndex = 25;
+            this.lblIMQuan1.Text = "Quantity";
             // 
-            // label2
+            // lblIMItemCode1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 58);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 13);
-            this.label2.TabIndex = 24;
-            this.label2.Text = "Item Code";
+            this.lblIMItemCode1.AutoSize = true;
+            this.lblIMItemCode1.Location = new System.Drawing.Point(6, 58);
+            this.lblIMItemCode1.Name = "lblIMItemCode1";
+            this.lblIMItemCode1.Size = new System.Drawing.Size(55, 13);
+            this.lblIMItemCode1.TabIndex = 24;
+            this.lblIMItemCode1.Text = "Item Code";
             // 
-            // label1
+            // lblIMName1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 17);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 23;
-            this.label1.Text = "Name";
+            this.lblIMName1.AutoSize = true;
+            this.lblIMName1.Location = new System.Drawing.Point(6, 17);
+            this.lblIMName1.Name = "lblIMName1";
+            this.lblIMName1.Size = new System.Drawing.Size(35, 13);
+            this.lblIMName1.TabIndex = 23;
+            this.lblIMName1.Text = "Name";
             // 
-            // dataInv
+            // dataIMInv1
             // 
-            this.dataInv.AllowUserToAddRows = false;
-            this.dataInv.AllowUserToDeleteRows = false;
-            this.dataInv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataInv.Location = new System.Drawing.Point(43, 137);
-            this.dataInv.Name = "dataInv";
-            this.dataInv.ReadOnly = true;
-            this.dataInv.Size = new System.Drawing.Size(745, 316);
-            this.dataInv.TabIndex = 31;
+            this.dataIMInv1.AllowUserToAddRows = false;
+            this.dataIMInv1.AllowUserToDeleteRows = false;
+            this.dataIMInv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataIMInv1.Location = new System.Drawing.Point(43, 137);
+            this.dataIMInv1.Name = "dataIMInv1";
+            this.dataIMInv1.ReadOnly = true;
+            this.dataIMInv1.Size = new System.Drawing.Size(745, 316);
+            this.dataIMInv1.TabIndex = 31;
             // 
-            // btnAdd
+            // btnIMAdd1
             // 
-            this.btnAdd.Location = new System.Drawing.Point(571, 48);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(98, 23);
-            this.btnAdd.TabIndex = 21;
-            this.btnAdd.Text = "Add New Item";
-            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnIMAdd1.Location = new System.Drawing.Point(571, 48);
+            this.btnIMAdd1.Name = "btnIMAdd1";
+            this.btnIMAdd1.Size = new System.Drawing.Size(98, 23);
+            this.btnIMAdd1.TabIndex = 21;
+            this.btnIMAdd1.Text = "Add New Item";
+            this.btnIMAdd1.UseVisualStyleBackColor = true;
             // 
-            // txtdisc
+            // txtIMdisc1
             // 
-            this.txtdisc.Location = new System.Drawing.Point(359, 74);
-            this.txtdisc.Name = "txtdisc";
-            this.txtdisc.Size = new System.Drawing.Size(100, 20);
-            this.txtdisc.TabIndex = 20;
+            this.txtIMdisc1.Location = new System.Drawing.Point(359, 74);
+            this.txtIMdisc1.Name = "txtIMdisc1";
+            this.txtIMdisc1.Size = new System.Drawing.Size(100, 20);
+            this.txtIMdisc1.TabIndex = 20;
             // 
-            // txtprc
+            // txtIMprc1
             // 
-            this.txtprc.Location = new System.Drawing.Point(359, 33);
-            this.txtprc.Name = "txtprc";
-            this.txtprc.Size = new System.Drawing.Size(100, 20);
-            this.txtprc.TabIndex = 19;
+            this.txtIMprc1.Location = new System.Drawing.Point(359, 33);
+            this.txtIMprc1.Name = "txtIMprc1";
+            this.txtIMprc1.Size = new System.Drawing.Size(100, 20);
+            this.txtIMprc1.TabIndex = 19;
             // 
-            // txtCol
+            // txtIMcol1
             // 
-            this.txtCol.Location = new System.Drawing.Point(200, 74);
-            this.txtCol.Name = "txtCol";
-            this.txtCol.Size = new System.Drawing.Size(100, 20);
-            this.txtCol.TabIndex = 18;
+            this.txtIMcol1.Location = new System.Drawing.Point(200, 74);
+            this.txtIMcol1.Name = "txtIMcol1";
+            this.txtIMcol1.Size = new System.Drawing.Size(100, 20);
+            this.txtIMcol1.TabIndex = 18;
             // 
-            // txtQuan
+            // txtIMQuan1
             // 
-            this.txtQuan.Location = new System.Drawing.Point(200, 33);
-            this.txtQuan.Name = "txtQuan";
-            this.txtQuan.Size = new System.Drawing.Size(100, 20);
-            this.txtQuan.TabIndex = 17;
+            this.txtIMQuan1.Location = new System.Drawing.Point(200, 33);
+            this.txtIMQuan1.Name = "txtIMQuan1";
+            this.txtIMQuan1.Size = new System.Drawing.Size(100, 20);
+            this.txtIMQuan1.TabIndex = 17;
             // 
-            // txtCode
+            // txtIMCode1
             // 
-            this.txtCode.Location = new System.Drawing.Point(6, 74);
-            this.txtCode.Name = "txtCode";
-            this.txtCode.Size = new System.Drawing.Size(144, 20);
-            this.txtCode.TabIndex = 16;
+            this.txtIMCode1.Location = new System.Drawing.Point(6, 74);
+            this.txtIMCode1.Name = "txtIMCode1";
+            this.txtIMCode1.Size = new System.Drawing.Size(144, 20);
+            this.txtIMCode1.TabIndex = 16;
             // 
-            // txtName
+            // txtIMName1
             // 
-            this.txtName.Location = new System.Drawing.Point(6, 33);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(144, 20);
-            this.txtName.TabIndex = 15;
+            this.txtIMName1.Location = new System.Drawing.Point(6, 33);
+            this.txtIMName1.Name = "txtIMName1";
+            this.txtIMName1.Size = new System.Drawing.Size(144, 20);
+            this.txtIMName1.TabIndex = 15;
             // 
-            // groupBox1
+            // grpIMadd
             // 
-            this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.btnAdd);
-            this.groupBox1.Controls.Add(this.txtdisc);
-            this.groupBox1.Controls.Add(this.txtprc);
-            this.groupBox1.Controls.Add(this.txtCol);
-            this.groupBox1.Controls.Add(this.txtQuan);
-            this.groupBox1.Controls.Add(this.txtCode);
-            this.groupBox1.Controls.Add(this.txtName);
-            this.groupBox1.Location = new System.Drawing.Point(43, 21);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(745, 110);
-            this.groupBox1.TabIndex = 33;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Add New Item";
+            this.grpIMadd.Controls.Add(this.lblIMdisc1);
+            this.grpIMadd.Controls.Add(this.lblIMprice1);
+            this.grpIMadd.Controls.Add(this.lblIMCol1);
+            this.grpIMadd.Controls.Add(this.lblIMQuan1);
+            this.grpIMadd.Controls.Add(this.lblIMItemCode1);
+            this.grpIMadd.Controls.Add(this.lblIMName1);
+            this.grpIMadd.Controls.Add(this.btnIMAdd1);
+            this.grpIMadd.Controls.Add(this.txtIMdisc1);
+            this.grpIMadd.Controls.Add(this.txtIMprc1);
+            this.grpIMadd.Controls.Add(this.txtIMcol1);
+            this.grpIMadd.Controls.Add(this.txtIMQuan1);
+            this.grpIMadd.Controls.Add(this.txtIMCode1);
+            this.grpIMadd.Controls.Add(this.txtIMName1);
+            this.grpIMadd.Location = new System.Drawing.Point(43, 21);
+            this.grpIMadd.Name = "grpIMadd";
+            this.grpIMadd.Size = new System.Drawing.Size(745, 110);
+            this.grpIMadd.TabIndex = 33;
+            this.grpIMadd.TabStop = false;
+            this.grpIMadd.Text = "Add New Item";
             // 
             // InventoryMan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(836, 508);
-            this.Controls.Add(this.btnUpdate);
-            this.Controls.Add(this.dataInv);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnIMUpdate1);
+            this.Controls.Add(this.dataIMInv1);
+            this.Controls.Add(this.grpIMadd);
             this.Name = "InventoryMan";
             this.Text = "Inventory Management";
-            ((System.ComponentModel.ISupportInitialize)(this.dataInv)).EndInit();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataIMInv1)).EndInit();
+            this.grpIMadd.ResumeLayout(false);
+            this.grpIMadd.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnUpdate;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataInv;
-        private System.Windows.Forms.Button btnAdd;
-        private System.Windows.Forms.TextBox txtdisc;
-        private System.Windows.Forms.TextBox txtprc;
-        private System.Windows.Forms.TextBox txtCol;
-        private System.Windows.Forms.TextBox txtQuan;
-        private System.Windows.Forms.TextBox txtCode;
-        private System.Windows.Forms.TextBox txtName;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnIMUpdate1;
+        private System.Windows.Forms.Label lblIMdisc1;
+        private System.Windows.Forms.Label lblIMprice1;
+        private System.Windows.Forms.Label lblIMCol1;
+        private System.Windows.Forms.Label lblIMQuan1;
+        private System.Windows.Forms.Label lblIMItemCode1;
+        private System.Windows.Forms.Label lblIMName1;
+        private System.Windows.Forms.DataGridView dataIMInv1;
+        private System.Windows.Forms.Button btnIMAdd1;
+        private System.Windows.Forms.TextBox txtIMdisc1;
+        private System.Windows.Forms.TextBox txtIMprc1;
+        private System.Windows.Forms.TextBox txtIMcol1;
+        private System.Windows.Forms.TextBox txtIMQuan1;
+        private System.Windows.Forms.TextBox txtIMCode1;
+        private System.Windows.Forms.TextBox txtIMName1;
+        private System.Windows.Forms.GroupBox grpIMadd;
     }
 }
 
